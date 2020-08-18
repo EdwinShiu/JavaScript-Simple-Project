3abe3005cfd4ce79157d70042c3af0ad6bca3585
Edwin Shiu <edwin2000412@gmail.com>
2020-08-18 19:22:05 +0800

    Reviews

--- Counter/counter.js
+++ Counter/counter.js
@@ -18,6 +18,15 @@ btns.forEach(function (btn) {
     else {
       count++;
     }
+    if (count > 0) {
+      value.style.color = 'green';
+    }
+    if (count < 0) {
+      value.style.color = 'red';
+    }
+    if (count == 0) {
+      value.style.color = '#222';
+    }
     value.textContent = count;
   });
 });
